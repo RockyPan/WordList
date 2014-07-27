69518090d86dd9623d8493b9e36ea240b971ce5a
RockyPan <pankkk@hotmail.com>
2014-07-27 15:20:40 +0800

    Initial Commit

---
+++ WordList/TWAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  TWAppDelegate.h
+//  WordList
+//
+//  Created by PanKyle on 14-7-27.
+//  Copyright (c) 2014年 TGD. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TWAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ WordList/TWViewController.m
@@ -0,0 +1,29 @@
+//
+//  TWViewController.m
+//  WordList
+//
+//  Created by PanKyle on 14-7-27.
+//  Copyright (c) 2014年 TGD. All rights reserved.
+//
+
+#import "TWViewController.h"
+
+@interface TWViewController ()
+
+@end
+
+@implementation TWViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
